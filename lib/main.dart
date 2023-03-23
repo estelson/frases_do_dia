6d5649d2d84862d7404aeb261619d6d64e734f09
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 09:05:35 -0300

    Finalização do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
 
 void main() {
@@ -30,11 +32,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<String> _frases = [
+    "Nas redes sociais o vizinho pode ter a grama mais verde mesmo não tendo quintal.",
+    "Reclama da mexerica descascada mas pede pra mamãe fazer seu prato.",
+    "Logo tudo será proibido, ainda bem que o proibido é mais gostoso.",
+    "Até de Plutão tem alguém se aproximando e você ai sem ninguém.",
+  ];
 
-  void _incrementCounter() {
+  String _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    // Obtém aleatoriamente uma frase da lista de frases
+    var numeroSorteado = Random().nextInt(_frases.length);
+
     setState(() {
-      _counter++;
+      _fraseGerada = _frases[numeroSorteado];
     });
   }
 
@@ -44,24 +56,45 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "You have pushed the button this many times:",
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/logo.png"),
+                  const SizedBox(height: 32),
+                  Text(
+                    _fraseGerada,
+                    textAlign: TextAlign.justify,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: _gerarFrase,
+                    child: const Text(
+                      "Nova frase",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              "$_counter",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: "Increment",
-        child: const Icon(Icons.add),
       ),
     );
   }
