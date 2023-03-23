fdde5317de4203c97775cbcb88dc33fb2c15a37e
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 07:17:32 -0300

    Limpeza e refatoração do código

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Frases do Dia',
+      title: "Frases do Dia",
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        // useMaterial3: true,
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Frases do Dia'),
+      home: const MyHomePage(title: "Frases do Dia"),
     );
   }
 }
@@ -44,7 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
@@ -52,10 +49,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              "You have pushed the button this many times:",
             ),
             Text(
-              '$_counter',
+              "$_counter",
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -63,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: "Increment",
         child: const Icon(Icons.add),
       ),
     );
